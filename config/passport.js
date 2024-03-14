4e3a94961c245e56928cbd8bb1d7a94c9f6c18f7
mhmddabdeltawab21 <mohamedabdeltawab338@gmail.com>
2024-03-14 14:21:46 +0200

    Created passport config

---
+++ config/passport.js
@@ -0,0 +1,34 @@
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+
+const verify = async (username, password, done) => {
+  const user = await User.findOne().where("name").equals(user.username).exec();
+
+  if (!user.errors()) {
+    return done(null, false, { message: "User not found" });
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    return done(null, false, { message: "Wrong username or password" });
+  }
+
+  return done(null, false);
+};
+
+const strategy = new localStrategy(verify);
+
+passport.use(strategy);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (user, done) => {
+  const user = await User.findOne().where("name").equals(user.username).exec();
+
+  done(null,user)
+});
